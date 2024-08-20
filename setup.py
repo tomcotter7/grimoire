72ed90e2cd116698d42caf347d56d2b1ad2da189
tomcotter7 <thomascotter00@gmail.com>
2024-08-20 16:49:52 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="grimoire",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A simple tool for extracting chunks from markdown files",
+    author="Thomas Cotter",
+    author_email="thomascotter00@gmail.com",
+)
